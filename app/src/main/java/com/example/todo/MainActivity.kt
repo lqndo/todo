3a9f9d80de081dc77e2ea47a8360c55996968672
Marco-Perez-Padilla <alu0101469348@ull.edu.es>
2024-08-15 17:30:11 +0100

    Add Home Screen Scaffold

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todo.components.TopAppBar
 import com.example.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +26,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Scaffold (
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            TopAppBar()
+                        },
+                        content = {
+                            Column (
+                                modifier = Modifier.padding(it)
+                            ) {
+                                Text(text = "Home")
+                            }
+                        }
+                    )
                 }
             }
         }
     }
 }
-
-
