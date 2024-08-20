70a7d7cf13223ae87e6b630ba144eb6382de0897
orlando <ninaorellanaorlando@gmail.com>
2024-08-20 20:33:15 +0100

    shit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.room.runtime)
-    implementation(libs.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
