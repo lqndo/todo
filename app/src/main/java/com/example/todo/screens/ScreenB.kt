20ed4a043b3c804deb8dcd98646a8cb37010f18d
cris-ez-dev <alu0101470856@ull.edu.es>
2024-08-22 16:20:54 +0100

    navigation added3

--- app/src/main/java/com/example/todo/screens/ScreenB.kt
+++ app/src/main/java/com/example/todo/screens/ScreenB.kt
@@ -6,18 +6,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.example.todo.Routes
+import com.example.todo.components.TopAppBar
 
-@OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
 fun ScreenB(
@@ -26,25 +31,36 @@ fun ScreenB(
     ) {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Screen B") })
+            TopAppBar(route = Routes.SCREEN_B,
+                navigateToHome = navigateToHome,
+                navigateToC = navigateToC)
+
         })
-    {
+    { it ->
         Column (
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = it.calculateTopPadding()),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Button(onClick = navigateToHome) {
-                Text(text = "Go to Home")
-            }
-            Spacer(modifier = Modifier.height(10.dp))
+            var task by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(value = task,
+                modifier = Modifier.height(70.dp),
+                onValueChange = {
+                    task = it
+                },
+                label = { Text(text = "Task") })
 
-            Button(onClick = navigateToC) {
-                Text(text = "Navigate to C")
-                
-            }
+            Spacer(modifier = Modifier.size(10.dp))
+
+            var body  by remember { mutableStateOf(TextFieldValue("")) }
+            OutlinedTextField(value = body,
+                modifier = Modifier.weight(1f),
+                onValueChange = {
+                    body = it
+                },
+                label = { Text(text = "Task Description") })
         }
     }
 }
--- app/src/main/java/com/example/todo/screens/ScreenC.kt
+++ app/src/main/java/com/example/todo/screens/ScreenC.kt
@@ -1,13 +1,20 @@
 package com.example.todo.screens
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,40 +22,64 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.todo.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
 fun ScreenC(
-    navigateToHome:()-> Unit,
-    navigateToB :()-> Unit
+    navigateToHome: () -> Unit,
+    navigateToB: () -> Unit
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(title = { Text(text = "Screen C ") })
-        })
-    {
-        Column (
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(top = it.calculateTopPadding()),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Button(onClick = navigateToHome) {
-                Text(text = "Go to Home")
-            }
-            Spacer(modifier = Modifier.height(10.dp))
+                .border(width = 4.dp, color = Color.Gray, shape = RoundedCornerShape(16.dp))
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
 
-            Button(onClick = navigateToB) {
-                Text(text = "Navigate to B")
+                Text(
+                    text = "This text is written",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
 
+
+                Row(
+                    Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                        Icon(
+                        modifier = Modifier.size(32.dp),
+                        painter = painterResource(id = R.drawable.ic_cancel),
+                        contentDescription = "Back"
+                    )
+
+                    Icon(
+                        modifier = Modifier.size(50.dp),
+                        painter = painterResource(id = R.drawable.ic_ok),
+                        contentDescription = "Delete"
+                    )
+                }
             }
         }
     }
 }
-
 @Preview
 @Composable
 fun PreviewScreenC(){
