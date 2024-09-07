9cf2698815ca6462da2f249e6aa3b3077da535a1
orlando <ninaorellanaorlando@gmail.com>
2024-09-07 16:07:43 +0100

    remove scaffold form note and home screen

--- app/src/main/java/com/example/todo/screens/Home.kt
+++ app/src/main/java/com/example/todo/screens/Home.kt
@@ -1,37 +1,21 @@
 package com.example.todo.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.todo.Routes
 import com.example.todo.ToDo
 import com.example.todo.components.ToDoCard
-import com.example.todo.components.TopAppBar
 
 @Composable
 fun HomeScreen(todoList: List<ToDo>, navigateToNote: () -> Unit) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                route = Routes.HOME,
-                navigateToHome = {},
-                navigateToNote = navigateToNote
-            )
-        }
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
     ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
-        ) {
-            items(todoList) { todo ->
-                ToDoCard(title = todo.title, content = todo.content)
-            }
+        items(todoList) { todo ->
+            ToDoCard(title = todo.title, content = todo.content)
         }
     }
 }
--- app/src/main/java/com/example/todo/screens/Note.kt
+++ app/src/main/java/com/example/todo/screens/Note.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,66 +20,53 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.todo.Routes
-import com.example.todo.components.TopAppBar
 
 @Composable
 fun NoteScreen(title: String, content: String, navigateToHome: () -> Unit) {
     var titleText by remember { mutableStateOf(TextFieldValue("")) }
     var contentText by remember { mutableStateOf(TextFieldValue("")) }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                route = Routes.NOTE,
-                navigateToHome = navigateToHome,
-                navigateToNote = {}
-            )
-        }
-    ) { it ->
-        Column(
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF1D1C22))
+            .padding(15.dp)
+    ) {
+        OutlinedTextField(
+            value = titleText,
             modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-                .background(Color(0xFF1D1C22))
-                .padding(15.dp)
-        ) {
-            OutlinedTextField(
-                value = titleText,
-                modifier = Modifier
-                    .height(70.dp)
-                    .fillMaxWidth(),
-                onValueChange = {
-                    titleText = it
-                },
-                label = {
-                    Text(
-                        text = "Title",
-                        color = Color.White
-                    )
-                },
-                colors = OutlinedTextFieldDefaults.colors(Color.White)
-            )
+                .height(70.dp)
+                .fillMaxWidth(),
+            onValueChange = {
+                titleText = it
+            },
+            label = {
+                Text(
+                    text = "Title",
+                    color = Color.White
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(Color.White)
+        )
 
-            Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
-            OutlinedTextField(
-                value = contentText,
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth(),
-                onValueChange = {
-                    contentText = it
-                },
-                label = {
-                    Text(
-                        text = "Content",
-                        color = Color.White
-                    )
-                },
-                colors = OutlinedTextFieldDefaults.colors(Color.White)
-            )
-        }
+        OutlinedTextField(
+            value = contentText,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            onValueChange = {
+                contentText = it
+            },
+            label = {
+                Text(
+                    text = "Content",
+                    color = Color.White
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(Color.White)
+        )
     }
 }
 
