7a4b9c00c27fcd69a11c5760a43fdd2d4e167072
orlando <ninaorellanaorlando@gmail.com>
2024-09-07 16:05:33 +0100

    add scaffold to mainactivity

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.todo.components.TopAppBar
 import com.example.todo.screens.HomeScreen
 import com.example.todo.screens.NoteScreen
 import com.example.todo.ui.theme.TodoTheme
@@ -16,28 +21,45 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TodoTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = Routes.HOME) {
-                    composable(
-                        route = Routes.HOME
-                    ) {
-                        HomeScreen(
-                            todoList = emptyList(),
-                            navigateToNote = {
-                                navController.navigate(Routes.NOTE)
-                            }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            route = Routes.HOME,
+                            navigateToHome = {},
+                            navigateToNote = {}
                         )
                     }
-                    composable(
-                        route = Routes.NOTE
+                ) { paddings ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        modifier = Modifier.padding(paddings),
+                        navController = navController,
+                        startDestination = Routes.HOME
                     ) {
-                        NoteScreen(
-                            title = "Title",
-                            content = "Content",
-                            navigateToHome = {
-                                navController.popBackStack()
-                            }
-                        )
+                        composable(
+                            route = Routes.HOME
+                        ) {
+                            HomeScreen(
+                                todoList = emptyList(),
+                                navigateToNote = {
+                                    navController.navigate(Routes.NOTE)
+                                }
+                            )
+                        }
+
+                        composable(
+                            route = Routes.NOTE
+                        ) {
+                            NoteScreen(
+                                title = "Title",
+                                content = "Content",
+                                navigateToHome = {
+                                    navController.popBackStack()
+                                }
+                            )
+                        }
                     }
                 }
             }
