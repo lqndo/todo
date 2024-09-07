59ea33471a04770403e5a2baea610ab3565147d7
orlando <ninaorellanaorlando@gmail.com>
2024-09-07 23:16:14 +0100

    add toDomain fun

--- app/src/main/java/com/example/todo/database/TodoEntity.kt
+++ app/src/main/java/com/example/todo/database/TodoEntity.kt
@@ -2,6 +2,7 @@ package com.example.todo.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.todo.ToDo
 
 @Entity(tableName = "todo")
 data class TodoEntity(
@@ -9,4 +10,9 @@ data class TodoEntity(
     val id: Int? = null,
     val title: String,
     val content: String
+)
+
+fun TodoEntity.toDomain() = ToDo(
+    title = this.title,
+    content = this.content
 )
