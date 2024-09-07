5ee8dc3b048196c6337e0852d7eacbf1fd1de0d8
orlando <ninaorellanaorlando@gmail.com>
2024-09-07 23:17:54 +0100

    add toEntity fun

--- app/src/main/java/com/example/todo/ToDo.kt
+++ app/src/main/java/com/example/todo/ToDo.kt
@@ -1,6 +1,13 @@
 package com.example.todo
 
+import com.example.todo.database.TodoEntity
+
 data class ToDo(
     val title: String,
     val content: String
+)
+
+fun ToDo.toEntity() = TodoEntity(
+    title = this.title,
+    content = this.content
 )
